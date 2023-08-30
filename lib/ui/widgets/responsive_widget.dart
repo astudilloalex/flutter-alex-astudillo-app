cabacb5738fdf6227ab17ef583cc4ce119a5f34a
Alex Astudillo <astudilloalex1@gmail.com>
2023-08-29 23:32:08 -0500

    Add responsive widget

---
+++ lib/ui/widgets/responsive_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  const ResponsiveWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
