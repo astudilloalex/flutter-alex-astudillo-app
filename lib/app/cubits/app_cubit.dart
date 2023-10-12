27dabf4f74ce638e2bdb257a1b2eb0fe48ec3f9e
Alex Astudillo <astudilloalex1@gmail.com>
2023-10-12 06:39:25 -0500

    feat: Add app cubit

---
+++ lib/app/cubits/app_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:alex_astudillo/app/states/app_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppCubit extends Cubit<AppState> {
+  AppCubit() : super(const AppState());
+}
---
+++ lib/app/states/app_state.dart
@@ -0,0 +1,3 @@
+class AppState {
+  const AppState();
+}
